fd07a90d8e91c9efc3030c6dc47c40e5dea09f39
zcemycl <lyc010197@gmail.com>
2023-11-14 22:03:14 +0000

    update to try embeddings on other factors

--- src/docker/postgres/init.sql
+++ src/docker/postgres/init.sql
@@ -7,3 +7,22 @@ CREATE TABLE IF NOT EXISTS record (
 	id bigint primary key,
 	factors VECTOR(1536)
 );
+
+CREATE TABLE IF NOT EXISTS color (
+	id bigint primary key,
+	name varchar(255) unique,
+	factors VECTOR(1536)
+);
+
+CREATE TABLE IF NOT EXISTS pattern (
+	id bigint primary key,
+	name varchar(255) unique,
+	factors VECTOR(1536)
+);
+
+CREATE TABLE IF NOT EXISTS garment (
+	id bigint primary key,
+	name varchar(255) unique,
+	factors VECTOR(1536),
+	path ltree
+);
--- src/qa_package/dataclasses/orm.py
+++ src/qa_package/dataclasses/orm.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
 from pgvector.sqlalchemy import Vector
-from sqlalchemy import BigInteger, Column
+from sqlalchemy import BigInteger, Column, String
 from sqlalchemy.orm import declarative_base
+from sqlalchemy.utils import LtreeType
 
 Base = declarative_base()
 metadata = Base.metadata
@@ -17,13 +18,20 @@ class record(Base):
 class color(Base):
     __tablename__ = "color"
     id = Column("id", BigInteger, primary_key=True)  # article_id is biginteger
+    name = Column("name", String, unique=True)
+    factors = Column("factors", Vector(1536))  # vector dimension = 1536
 
 
 class pattern(Base):
     __tablename__ = "pattern"
     id = Column("id", BigInteger, primary_key=True)  # article_id is biginteger
+    name = Column("name", String, unique=True)
+    factors = Column("factors", Vector(1536))  # vector dimension = 1536
 
 
 class garment(Base):
     __tablename__ = "garment"
     id = Column("id", BigInteger, primary_key=True)  # article_id is biginteger
+    name = Column("name", String, unique=True)
+    factors = Column("factors", Vector(1536))  # vector dimension = 1536
+    path = Column("path", LtreeType)
